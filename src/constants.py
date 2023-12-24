46dfc05797149175c68dbbb18a598632a90f5617
Raul Campos <raulcamp@mit.edu>
2023-12-23 16:54:42 -0800

    separated constants from main

---
+++ src/constants.py
@@ -0,0 +1,16 @@
+"""Game of Life constants"""
+# Dimensions
+WIDTH, HEIGHT = 1000, 1000
+GRID_SIZE = 25
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (50, 50, 50)
+RED = (240, 0, 0)
+BLUE = (0, 0, 240)
+CYAN = (0, 240, 240)
+GREEN = (0, 240, 0)
+YELLOW = (240, 240, 0)
+ORANGE = (240, 160, 0)
+PURPLE = (160, 0, 240)
